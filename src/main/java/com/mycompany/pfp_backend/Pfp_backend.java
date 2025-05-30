c66ababec5e68665c71a1f87075c9db06670f54f
Rael Souza da Paixão <rsouza2123@gmail.com>
2025-05-30 11:18:05 -0300

    Iniciando projeto

---
+++ src/main/java/com/mycompany/pfp_backend/Pfp_backend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pfp_backend;
+
+/**
+ *
+ * @author rsouz
+ */
+public class Pfp_backend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
