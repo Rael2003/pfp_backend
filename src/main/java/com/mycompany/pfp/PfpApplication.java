2cc6e232c973724b86b320a48d6662fbad43e02e
Rael Souza da Paixão <rsouza2123@gmail.com>
2025-06-02 12:10:58 -0300

    Spring Boot

---
+++ src/main/java/com/mycompany/pfp/PfpApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.pfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/pfp/PfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.pfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
